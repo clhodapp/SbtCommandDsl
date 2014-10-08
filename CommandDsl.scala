c27b94d31fb4e96fbfa7a2b09605da077f5bddc5
clhodapp <clhodapp1@gmail.com>
2014-10-08 02:29:19 -0700

    Add UnsafePretty extensions

--- CommandDsl.scala
+++ CommandDsl.scala
@@ -1,82 +1,94 @@
 
 import sbt._
 
-object StateM {
-  case class State[S, A](run: S => (S, A)) {
-    def map[B](f: A => B): State[S, B] = State { s =>
+trait StateM[S] {
+  case class State[A](run: S => (S, A)) {
+    def map[B](f: A => B): State[B] = State { s =>
       val (newS, a) = run(s)
       (newS, f(a))
     }
-    def flatMap[B](f: A => State[S, B]): State[S, B] = State { (s: S) =>
+    def flatMap[B](f: A => State[B]): State[B] = State { (s: S) =>
       val (newS, a) = run(s)
       f(a).run(newS)
     }
+		def filter(f: A => Boolean)(implicit fa: UnsafePretty.FiltersAllowed.type): State[A] = State { s =>
+			val (newState, a) = run(s)
+			if (f(a)) (s, a)
+			else throw new Exception("It's supposed to be irrefutable, you fool")
+		}
+		def withFilter(f: A => Boolean)(implicit fa: UnsafePretty.FiltersAllowed.type): State[A] = filter(f)
   }
-  def get[S]: State[S, S] = State(s => (s, s))
-  def put[S](s: S): State[S, Unit] = State(_ => (s, ()))
-  def unit[S, A](a: A): State[S, A] = State(s => (s, a))
+	object State {
+		def get: State[S] = State(s => (s, s))
+		def put(s: S): State[Unit] = State(_ => (s, ()))
+		def unit[A](a: A): State[A] = State(s => (s, a))
+	}
+	object UnsafePretty {
+		implicit def everythingIsAState[A](a: A): State[A] = State.unit(a)
+		implicit object FiltersAllowed
+	}
 }
 
-object CommandDsl {
 
-  type St[A] = StateM.State[sbt.State, A]
+object CommandDsl extends StateM[sbt.State] {
 
-  private[this] def extracted[T](f: sbt.Extracted => T): St[T] = StateM.get.map { sbtState =>
+	import UnsafePretty._
+
+  private[this] def extracted[T](f: sbt.Extracted => T): State[T] = State.get.map { sbtState =>
     f(Project.extract(sbtState))
   }
 
-  def append(settings: Seq[Def.Setting[_]]): St[Unit] = for {
-    sbtState <- StateM.get[sbt.State]
-    extracted <- StateM.unit(Project.extract(sbtState))
-    _ <- StateM.put(extracted.append(settings, sbtState))
+  def append(settings: Seq[Def.Setting[_]]): State[Unit] = for {
+    sbtState <- State.get
+    extracted <- Project.extract(sbtState)
+    _ <- State.put(extracted.append(settings, sbtState))
   } yield ()
 
-  def set(settings:Def.Setting[_]*): St[Unit] =
+  def set(settings:Def.Setting[_]*): State[Unit] =
     append(settings)
 
-  def currentLoader: St[ClassLoader] = extracted(_.currentLoader)
+  def currentLoader: State[ClassLoader] = extracted(_.currentLoader)
 
-  def currentProject: St[ResolvedProject] = extracted(_.currentProject)
+  def currentProject: State[ResolvedProject] = extracted(_.currentProject)
 
-  def currentRef: St[ProjectRef] = extracted(_.currentRef)
+  def currentRef: State[ProjectRef] = extracted(_.currentRef)
 
-  def currentUnit: St[LoadedBuildUnit] = extracted(_.currentUnit)
+  def currentUnit: State[LoadedBuildUnit] = extracted(_.currentUnit)
 
-  def get[T](key: SettingKey[T]): St[T] = extracted(_.get(key))
+  def get[T](key: SettingKey[T]): State[T] = extracted(_.get(key))
 
-  def get[T](key: TaskKey[T]): St[Task[T]] = extracted(_.get(key))
+  def get[T](key: TaskKey[T]): State[Task[T]] = extracted(_.get(key))
 
-  def getOpt[T](key: SettingKey[T]): St[Option[T]] = extracted(_.getOpt(key))
+  def getOpt[T](key: SettingKey[T]): State[Option[T]] = extracted(_.getOpt(key))
 
-  def rootProject[T]: St[URI => String] = extracted(_.rootProject)
+  def rootProject[T]: State[URI => String] = extracted(_.rootProject)
 
-  def runAggregated[T](key: TaskKey[T]): St[Unit] = for {
-    sbtState <- StateM.get[sbt.State]
-    extracted <- StateM.unit(Project.extract(sbtState))
-    newState <- StateM.unit(extracted.runAggregated(key, sbtState))
-    _ <- StateM.put(newState)
+  def runAggregated[T](key: TaskKey[T]): State[Unit] = for {
+    sbtState <- State.get
+    extracted <- Project.extract(sbtState)
+    newState <- extracted.runAggregated(key, sbtState)
+    _ <- State.put(newState)
   } yield ()
 
 
-  def runTask[T](key: TaskKey[T]): St[T] = for {
-    sbtState <- StateM.get[sbt.State]
-    extracted <- StateM.unit(Project.extract(sbtState))
-    r <- StateM.unit(extracted.runTask(key, sbtState))
-    (newState, result) = r
-    _ <- StateM.put(newState)
+  def runTask[T](key: TaskKey[T]): State[T] = for {
+    sbtState <- State.get
+    extracted <- Project.extract(sbtState)
+    (newState, result) <- extracted.runTask(key, sbtState)
+    _ <- State.put(newState)
   } yield result
 
-  def session: St[SessionSettings] = extracted(_.session)
+  def session: State[SessionSettings] = extracted(_.session)
 
-  def structure: St[BuildStructure] = extracted(_.structure)
+  def structure: State[BuildStructure] = extracted(_.structure)
 
-  def noArg(commandName: String)(f: StateM.State[sbt.State, Unit]): Command =
+  def noArg(commandName: String)(f: State[Unit]): Command =
     Command.command(commandName)(sbtState => f.run(sbtState)._1)
 
-  def singleArg(commandName: String)(f: String => StateM.State[sbt.State, Unit]): Command =
+  def singleArg(commandName: String)(f: String => State[Unit]): Command =
     Command.single(commandName)((sbtState, arg) => f(arg).run(sbtState)._1)
 
-  def multiArg(commandName: String, argsHint: String)(f: Seq[String] => StateM.State[sbt.State, Unit]): Command =
+  def multiArg(commandName: String, argsHint: String)(f: Seq[String] => State[Unit]): Command =
     Command.args(commandName, argsHint)((sbtState, args) => f(args).run(sbtState)._1)
 
 }
--- build.sbt
+++ build.sbt
@@ -6,3 +6,5 @@ name := "sbt-command-dsl"
 version := "0.0.1-SNAPSHOT"
 
 sbtPlugin := true
+
+scalacOptions += "-language:_"
